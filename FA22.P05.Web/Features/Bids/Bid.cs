11e4bea8f5f10f6e5c3fa2f4b56de3da228101ff
Jdillon297 <JACOBDILLON119@GMAIL.COM>
2022-10-03 19:58:59 -0500

    feat/Added a Bid Table

---
+++ FA22.P05.Web/Features/Bids/Bid.cs
@@ -0,0 +1,15 @@
+﻿using FA22.P05.Web.Features.Authorization;
+using FA22.P05.Web.Features.Listings;
+
+namespace FA22.P05.Web.Features.Bids
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual User? User { get; set; }
+        public decimal BidAmount { get; set; }
+        public virtual Listing? Listing {get; set;}
+        public int ListingId { get; set; }
+    }
+}
---
+++ FA22.P05.Web/Features/Bids/BidConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace FA22.P05.Web.Features.Bids
+{
+    public class BidConfiguration : IEntityTypeConfiguration<Bid>
+    {
+        public void Configure(EntityTypeBuilder<Bid> builder)
+        {
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .HasForeignKey(x => x.ListingId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+    }
+}
